229123bde0dd922842072f6cf59f9afdb380128d
Stanislav Červeňák <stano@wame.sk>
2016-08-04 10:39:04 +0200

    PATCH from OG 4

--- components/TextBlockControl/TextBlockControl.php
+++ components/TextBlockControl/TextBlockControl.php
@@ -2,7 +2,10 @@
 
 namespace Wame\TextBlockModule\Components;
 
+use Nette\DI\Container;
+use Wame\Core\Components\BaseControl;
 use Wame\TextBlockModule\Repositories\TextBlockRepository;
+
 
 interface ITextBlockControlFactory
 {
@@ -11,13 +14,16 @@ interface ITextBlockControlFactory
 }
 
 
-class TextBlockControl extends \Wame\Core\Components\BaseControl
+class TextBlockControl extends BaseControl
 {	
 	/** @var TextBlockControl */
 	private $textBlockRepository;
+    
 	
-	public function __construct(\Nette\DI\Container $container, TextBlockRepository $textBlockRepository) 
-	{
+	public function __construct(
+        Container $container, 
+        TextBlockRepository $textBlockRepository
+    ) {
 		parent::__construct($container);
 		
 		$this->textBlockRepository = $textBlockRepository;
@@ -26,25 +32,11 @@ class TextBlockControl extends \Wame\Core\Components\BaseControl
 	
 	public function render()
 	{
-		$show = true;
-
-        if(!$this->componentInPosition) {
-            throw new \Nette\InvalidArgumentException("TextBlockControl can only be used in position!");
-        }
-        
         $textBlock = $this->textBlockRepository->get(['component' => $this->componentInPosition->component]);
 
-        if ($textBlock && $textBlock->text) {
-            $this->template->position = $this->componentInPosition;
-            $this->template->component = $this->componentInPosition->component;
-            $this->template->textBlock = $textBlock;
-            $this->template->title = $this->getTitle();
-            $this->template->text = $textBlock->getText();
-        } else {
-            $show = false;
-        }
-
-		$this->template->show = $show;
+        $this->template->textBlock = $textBlock;
+        $this->template->title = $this->componentInPosition->component->title;
+        $this->template->text = $textBlock->getText();
 	}
 
 }
